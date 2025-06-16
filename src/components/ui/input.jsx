9f429f628f464291cf312dcbd23996b9f00b162b
smgalvao <59588342+smgalvao@users.noreply.github.com>
2025-06-16 18:22:47 -0300

    Create input.jsx

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,8 @@
+export function Input({ className = '', ...props }) {
+  return (
+    <input
+      className={`border rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-300 ${className}`}
+      {...props}
+    />
+  );
+}
