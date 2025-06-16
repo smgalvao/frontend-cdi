c9b86399ca45ee6ebe2807e8909f574bc0eec492
smgalvao <59588342+smgalvao@users.noreply.github.com>
2025-06-16 18:34:41 -0300

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,4 +1,6 @@
-export function Input({ className = '', ...props }) {
+import React from "react";
+
+export function Input({ className = "", ...props }) {
   return (
     <input
       className={`border rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-300 ${className}`}
